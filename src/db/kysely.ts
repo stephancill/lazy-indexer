17868b19f7842d9831f6a2dd4e218071f8418aa5
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-04-03 00:52:21 -0400

    Revert "Support Neon postgres"
    This reverts commit 4c1cbb825fc9c922b7b2fb0cc2e43a0645daf255.

--- src/db/kysely.ts
+++ src/db/kysely.ts
@@ -1,29 +1,18 @@
 import { CamelCasePlugin, Kysely, PostgresDialect } from 'kysely'
-import { NeonDialect } from 'kysely-neon'
 import Pool from 'pg-pool'
-import ws from 'ws'
 
 import { Tables } from './db.types'
 
-const DATABASE_URL = process.env.DATABASE_URL
-if (!DATABASE_URL) {
+if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not set')
 }
 
-const basicPostgresDialect = new PostgresDialect({
-  pool: new Pool({
-    connectionString: DATABASE_URL,
-  }),
-})
-
-const neonDialect = new NeonDialect({
-  connectionString: DATABASE_URL,
-  webSocketConstructor: ws,
-})
-
 export const db = new Kysely<Tables>({
-  dialect: DATABASE_URL.includes('neon.tech')
-    ? neonDialect
-    : basicPostgresDialect,
+  dialect: new PostgresDialect({
+    pool: new Pool({
+      max: 10,
+      connectionString: process.env.DATABASE_URL,
+    }),
+  }),
   plugins: [new CamelCasePlugin()],
 })
