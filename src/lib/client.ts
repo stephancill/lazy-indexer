1b07dc6e3995fbad654763e42bce35949fe3d486
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-04-10 00:38:05 -0400

    Support non-SSL hubs

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,9 +1,16 @@
-import { getSSLHubRpcClient } from '@farcaster/hub-nodejs'
+import {
+  getInsecureHubRpcClient,
+  getSSLHubRpcClient,
+} from '@farcaster/hub-nodejs'
 
 const HUB_RPC = process.env.HUB_RPC
+const HUB_SSL = process.env.HUB_SSL || 'true'
 
 if (!HUB_RPC) {
   throw new Error('HUB_RPC env variable is not set')
 }
 
-export const client = getSSLHubRpcClient(HUB_RPC)
+export const client =
+  HUB_SSL === 'true'
+    ? getSSLHubRpcClient(HUB_RPC)
+    : getInsecureHubRpcClient(HUB_RPC)
