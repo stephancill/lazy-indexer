e704aee3a2263c0b012a3df7ad1b3990e9803bbb
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-05-05 14:49:12 -0700

    Add stats endpoint

--- src/lib/express.ts
+++ src/lib/express.ts
@@ -3,6 +3,7 @@ import { BullMQAdapter } from '@bull-board/api/bullMQAdapter.js'
 import { ExpressAdapter } from '@bull-board/express'
 import express from 'express'
 
+import { getLatestEvent } from '../api/event.js'
 import { backfillQueue } from './backfill.js'
 import { log } from './logger.js'
 import { streamQueue } from './subscriber.js'
@@ -11,11 +12,18 @@ export function initExpressApp() {
   const app = express()
   const serverAdapter = new ExpressAdapter()
 
+  app.listen(3001, () => {
+    log.info('Server started on http://localhost:3001')
+  })
+
   serverAdapter.setBasePath('/')
   app.use('/', serverAdapter.getRouter())
 
-  app.listen(3001, () => {
-    log.info('Server started on http://localhost:3001')
+  app.get('/stats', async (req, res) => {
+    const latestEventId = await getLatestEvent()
+    const isBackfillActive = (await backfillQueue.getActiveCount()) > 0
+
+    return res.status(200).json({ latestEventId, isBackfillActive })
   })
 
   createBullBoard({
