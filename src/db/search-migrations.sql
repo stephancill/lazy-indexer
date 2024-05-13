13be1fa9cb14d7e2dbef1266995beaf9d230cd1a
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-05-13 00:20:28 -0400

    Remove search function
    It's faster to call the function directly for some reason

--- src/db/search-migrations.sql
+++ src/db/search-migrations.sql
@@ -8,19 +8,3 @@ to_tsvector('english', text)) stored;
 -- Create an index on the fts column for faster searching
 CREATE INDEX casts_fts ON casts
 USING GIN (fts);
-
--- Create a search function
-CREATE OR REPLACE FUNCTION search_casts (query text)
-	RETURNS SETOF casts
-	AS $$
-BEGIN
-	RETURN QUERY
-	SELECT
-		*
-	FROM
-		casts
-	WHERE
-		fts @@ to_tsquery(query);
-END;
-$$
-LANGUAGE plpgsql;
