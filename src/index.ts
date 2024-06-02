ec092933f60ba46626c007b0634c2d67bd86e688
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-06-02 21:19:48 +0200

    fix: handle multiple scripts running at the same time

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,14 @@ import { backfill } from './lib/backfill.js'
 import { initExpressApp } from './lib/express.js'
 import { subscribe } from './lib/subscriber.js'
 
-initExpressApp()
+try {
+  initExpressApp()
+} catch (error) {
+  console.error(
+    'Failed to initialize express app, it may already be running.',
+    error
+  )
+}
 
 if (process.argv[2] === '--backfill') {
   await backfill({
