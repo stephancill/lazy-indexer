ee1b97fdf6f53945f842001e6b1c643d80888535
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-09-17 21:40:55 +0200

    fix: run migration in script (#2)
    * added compose
    
    * edit commands
    
    * edited commands
    
    * fix exec cmd
    
    * added sleep to await migration
    
    * show logs
    
    * added lazy-indexer
    
    * update
    
    * test fix
    
    * testFIx1
    
    * updated req
    
    * updated req
    
    * Update lazy indexer to run migrations on startup in TypeScript
    
    * fix: remove db steps from docker image publish
    
    ---------
    
    Co-authored-by: Gabriel Temtsen <gabrieltemtsen@gmail.com>
    Co-authored-by: Gabriel Temtsen <gabrieltemtsen@Gabriels-MacBook-Pro.local>

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import { getLatestEvent } from './api/event.js'
 import { backfill } from './lib/backfill.js'
 import { initExpressApp } from './lib/express.js'
 import { subscribe } from './lib/subscriber.js'
+import { migrateToLatest } from './db/migrator.js'
+
+migrateToLatest()
 
 if (process.argv[2] === '--backfill') {
   initExpressApp()
