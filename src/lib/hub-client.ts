1e35c6bf9cc82b9a79e4b3741cc2ce2dd85ac2c2
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-09-19 19:30:50 +0200

    fix: increase max message length to 2gb (??)

--- src/lib/hub-client.ts
+++ src/lib/hub-client.ts
@@ -6,7 +6,7 @@ import {
 import type { HubClient } from './types'
 
 // https://github.com/farcasterxyz/hub-monorepo/commit/fb54ef89bb4731002cf3d7f59c8e52b011e58310
-const MAX_RECEIVE_MESSAGE_LENGTH = 1000 * 1024 * 1024 // 1GB (??)
+const MAX_RECEIVE_MESSAGE_LENGTH = 1000 * 1024 * 1024 * 2 // 2GB (??)
 const defaultOptions = {
   'grpc.max_receive_message_length': MAX_RECEIVE_MESSAGE_LENGTH,
 }
